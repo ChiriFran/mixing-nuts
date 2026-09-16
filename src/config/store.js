b26f425bc653af6060758ae1e80cce906b628cb8
ChiriFran <franchiri00@gmail.com>
2026-09-16 13:46:34 -0300

    numero

--- src/config/store.js
+++ src/config/store.js
@@ -1,7 +1,7 @@
 export const storeConfig = {
   name: 'Mixing Nuts',
   tagline: 'Frutos secos y mixes seleccionados',
-  whatsapp: '5491130504515',
+  whatsapp: '5491138585573',
   email: 'info@mixingnuts.com.ar',
   instagram: 'https://instagram.com/mixingnuts',
   address: '',
